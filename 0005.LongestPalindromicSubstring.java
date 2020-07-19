a825760bbda8b6a2181857c43e893c0a3cc0380d
Bruce Bi <jianlu_bi@manulife.ca>
2020-07-19 00:25:53 -0400

    leetcode 5

---
+++ 0005.LongestPalindromicSubstring.java
@@ -0,0 +1,27 @@
+class Solution {
+    private int start = 0, maxLength = 1;
+
+    public String longestPalindrome(String s) {
+        if (s.length() < 2) {
+            return s;
+        }
+
+        for (int i = 0; i < s.length(); i++) {
+            expandAroundCenter(s, i - 1, i + 1);
+            expandAroundCenter(s, i, i + 1);
+        }
+
+        return s.substring(start, start + maxLength);
+    }
+
+    private void expandAroundCenter(String s, int left, int right) {
+        while (left >= 0 && right < s.length() && s.charAt(left) == s.charAt(right)) {
+            if (right - left + 1 > maxLength) {
+                maxLength = right - left + 1;
+                start = left;
+            }
+            left--;
+            right++;
+        }
+    }
+}
