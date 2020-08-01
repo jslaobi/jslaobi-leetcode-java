1509f6a64d65f6b7f1378f51627c9747cb530861
Bruce Bi <jianlu_bi@manulife.ca>
2020-08-01 02:07:36 -0400

    leetcode 83

---
+++ 0083.RemoveDuplicatesfromSortedList.java
@@ -0,0 +1,13 @@
+class Solution {
+    public ListNode deleteDuplicates(ListNode head) {
+    ListNode current = head;
+    while(current != null && current.next != null){
+        if(current.val == current.next.val){
+            current.next = current.next.next;
+        }else{
+            current = current.next;
+        }
+    }
+    return head;
+    }
+}
